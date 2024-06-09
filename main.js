9306fbb4b4b7ffac02124aac101b9e6c60bcc8b4
alona007 <alona.olyksashenko@gmail.com>
2024-06-09 20:11:44 +0200

    creation-header-and-hero

---
+++ main.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const openBtn = document.querySelector('.mobile-open-btn');
+    const closeBtn = document.querySelector('.mobile-menu-close-btn');
+    const mobileMenuContainer = document.querySelector('.mobile-menu-container');
+    const menuLinks = document.querySelectorAll('.mobile-menu-nav-link');
+  
+    function openMobileMenu() {
+      mobileMenuContainer.classList.remove('close');
+      mobileMenuContainer.classList.add('open');
+    }
+  
+    function closeMobileMenu() {
+      mobileMenuContainer.classList.remove('open');
+      mobileMenuContainer.classList.add('close');
+    }
+  
+    openBtn.addEventListener('click', openMobileMenu);
+    closeBtn.addEventListener('click', closeMobileMenu);
+  
+    menuLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        closeMobileMenu();
+      });
+    });
+  });
